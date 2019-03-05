220521b5740fcfbbf9991325db07fe859fea9968
Wojciech Lisiecki <wojciech.lisiecki@briisk.co>
2019-03-05 11:04:53 +0100

    change variants to flavors

--- src/environments/environment.default.js
+++ src/environments/environment.default.js
@@ -1,7 +1,7 @@
 import { baseEnvironment } from "./environment.base";
 
 export const whiteLabeledEnvironment = {
-  baseColor: '#ffffff',
+  baseColor: '#000000',
 };
 
 export const environment = Object.assign({}, baseEnvironment, whiteLabeledEnvironment);
